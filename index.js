c867a8ed158d820d86bfde1643588ff6bed6e73e
June238025$ <dorianquintana@gmail.com>
2023-08-21 06:26:12 -0700

    webgame part one

--- index.js
+++ index.js
@@ -4,3 +4,69 @@ greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)
+
+let pineTree = document.createElement('img')
+pineTree.src = 'assets/pine-tree.png'
+pineTree.style.position = 'fixed'
+pineTree.style.left = '450px'
+pineTree.style.bottom = '200px'
+document.body.append(pineTree)
+
+let tree = document.createElement('img')
+tree.src = 'assets/tree.png'
+tree.style.position = 'fixed'
+tree.style.left = '200px'
+tree.style.bottom = '300px'
+document.body.append(tree)
+
+let pillar = document.createElement ('img')
+pillar.src = 'assets/pillar.png'
+pillar.style.position = 'fixed'
+pillar.style.left = '350px'
+pillar.style.bottom = '100px'
+document.body.append(pillar)
+
+let crate = document.createElement ('img')
+crate.src = 'assets/crate.png'
+crate.style.position = 'fixed'
+crate.style.left = '150px'
+crate.style.bottom = '200px'
+document.body.append(crate)
+
+let well = document.createElement ('img')
+well.src = 'assets/well.png'
+well.style.position = 'fixed'
+well.style.left = '500px'
+well.style.bottom = '450px'
+document.body.append(well)
+
+
+
+
+
+let sword = document.createElement('img')
+sword.src = 'assets/sword.png'
+sword.position = 'fixed'
+sword.left = '500px'
+sword.bottom = '405px'
+document.body.append(sword)
+
+sword.addEventListener('click', function(){
+    sword.remove()
+})
+
+
+
+let shield = document.createElement('img')
+shield.src = 'assets/shield.png'
+shield.position = 'fixed'
+shield.left = '165px'
+shield.bottom = '185px'
+document.body.append(shield)
+
+let staff = document.createElement('img')
+staff.src = 'assets/staff.png'
+staff.left = '600px'
+staff.bottom = '100px'
+document.body.append(staff)
+
